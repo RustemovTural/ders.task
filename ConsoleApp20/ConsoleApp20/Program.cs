cb02adb04a2aa5acdacde3f15adacbf0f62e2e9f
tu7n8gcpt@code.edu.az <tu81s0a6x@code.edu.az>
2024-04-02 09:22:23 +0400

    olan balimizda kesilecek

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool exit = true;
+            Weapon weapon1=new Weapon(12,6,true);
+            do
+            {
+                Console.Write("Seçiminizi daxil edin");
+                Console.WriteLine("0 - İnformasiya almaq üçün");
+                Console.WriteLine("1 - Ateş açmaq üçün");
+                Console.WriteLine("2 - GetRemainBulletCount metodu üçün");
+                Console.WriteLine("3 - Reload metodu üçün");
+                Console.WriteLine("4 - ChangeFireMode metodu üçün");
+                Console.WriteLine("5 - Proqramdan çıxmaq üçün");
+                string input= Console.ReadLine();
+                switch(input)
+                {
+                    case "0":
+                     
+                        break;
+                        
+                    case "1":weapon1.Shoot();
+                        break;
+                    case "2":
+                        Console.WriteLine("Qalan gülə sayı: " + weapon1.GetRemainBulletCount());
+                        break;
+                    case "3":weapon1.Reload();
+                        break;
+                    case "4":weapon1.ChangeFireMode();
+                        break;
+                    case "5":
+                        exit=true;
+                        
+                        return;
+                    default:Console.WriteLine("duzgun secim edin");
+                        break;
+                        
+
+
+                };
+                   
+            }while (exit!=true);
+            
+             
+        }
+    }
+}
---
+++ ConsoleApp20/ConsoleApp20/Weapon.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Weapon
+    {
+        public int CapacityBullet {  get; set; }
+        public int CurrentBullet { get; set;}
+        public bool FireMood { get; set; }
+        public Weapon(int capacitybullet,int currentbullet,bool firemood)
+        {
+            CapacityBullet = capacitybullet;
+            CurrentBullet = currentbullet;
+            FireMood = firemood;
+        }public void Shoot() 
+        {
+            if (CurrentBullet > 0)
+            {
+                if (FireMood = true)
+                {
+                    Console.WriteLine("Avtomatik atis");
+                }
+                else
+                {
+                    Console.WriteLine("Tekli atis");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("gulle yoxdur");
+            }
+
+        }
+        public int GetRemainBulletCount()
+        {
+            return CapacityBullet;
+        }
+        public void Reload()
+        {
+            CapacityBullet = CurrentBullet;
+            Console.WriteLine("Gulle dolduruldu");
+        }
+        public void ChangeFireMode()
+        {
+            if(FireMood = true)
+            {
+                Console.WriteLine("avtoamtik atis");
+            }
+            else
+            {
+                Console.WriteLine("tekli atis");
+            }
+        }
+
+    }
+    
+}
